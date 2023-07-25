b6833a29afc5ec180212488899d3361deb481c1a
digimori <hanaorikuu@gmail.com>
2023-07-25 12:46:37 +0000

    feat: Videos component file added - Not yet populated. Imported into Feed component.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { Directions } from '@mui/icons-material';
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -13,6 +13,18 @@ const Feed = () => {
                       variant="body2"
                       sx={{ mt:1/5, color: '#fff'}}>CR - Boo Toob 2023</Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: '#fff' }}>
+         New <span style={{ color: '#fc1503' }}>Videos</span>
+        </Typography>
+        <Videos />
+
+
+      </Box>
+
+
+
     </Stack>
   )
 }
