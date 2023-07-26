7901dcd08e929bfe099e00f956be8dd42f552947
digimori <hanaorikuu@gmail.com>
2023-07-26 13:40:57 +0000

    feat: Videodetails component and Search feed component implemented. Fixed margins on smaller devices for VideoDetail page.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -14,12 +14,12 @@ const Feed = () => {
   }, [selectedCategory]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: {sx: 0, md: 2 } }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
       <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
           <Typography className="copyright"
                       variant="body2"
-                      sx={{ mt:1/5, color: '#fff'}}>CR - Boo Toob 2023</Typography>
+                      sx={{ mt:1.5, color: '#fff'}}>CR - Boo Toob 2023</Typography>
       </Box>
 
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,27 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com'; /* So in theory, we can env.js this, right? Secret keys? */
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com',
   params: {
-    part: 'snippet',
-    videoId: 'M7FIvfx5J10',
     maxResults: 50,
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
 
 /* Below allows the use of data as a prop across components after it has fetched the data from the url/API */
 
