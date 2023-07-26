749e0ae4aa37f32b6c7a357e2ddeee86b8a4ae6a
digimori <hanaorikuu@gmail.com>
2023-07-26 11:17:16 +0000

    feat: Video cards rendered - Video details not yet compiled

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stack, Box } from '@mui/material';
-import { VideoCard, ChannelDetail, ChannelCard } from './';
+import { VideoCard, ChannelCard } from './';
 
 
 const Videos = ({ videos }) => {
@@ -10,7 +10,7 @@ const Videos = ({ videos }) => {
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
+         {/*{item.id.channelId && <ChannelCard channelDetail={item} />}*/} 
         </Box>
       ))}
     </Stack>
